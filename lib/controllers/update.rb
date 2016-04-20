8c697961050dde796830fc3e8ec18516f3e7e87d
Яyan Priebe <rpriebe@me.com>
2016-04-20 14:10:58 -0600

    added a mostly complete update controller, just need to have actual data to process now

--- lib/controllers/update.rb
+++ lib/controllers/update.rb
@@ -2,8 +2,40 @@ module Vermillion
   module Controller
     class Update < Controller::Base
 
-      def default
-        puts "hi"
+      SERVERS = ['192.168.0.74', 'localhost']
+
+      def all
+        SERVERS.each do |srv|
+          https_endpoint = URI.parse("http://#{srv}")
+
+          begin
+            resp = Net::HTTP.get_response(https_endpoint)
+
+            # we don't really care why this failed, just that it did
+            raise Errno::ECONNREFUSED if resp.code.to_i > 399
+
+            # handle JSON response
+            # JSON.parse(resp.body)
+          rescue Errno::ECONNREFUSED => e
+            Notify.warning("Request failed for #{https_endpoint}")
+          end
+        end
+      end
+
+      def one(srv)
+        https_endpoint = URI.parse("http://#{srv}")
+
+        begin
+          resp = Net::HTTP.get_response(https_endpoint)
+
+          # we don't really care why this failed, just that it did
+            raise Errno::ECONNREFUSED if resp.code.to_i > 399
+
+          # handle JSON response
+          # JSON.parse(resp.body)
+        rescue Errno::ECONNREFUSED => e
+          Notify.warning("Request failed for #{https_endpoint}")
+        end
       end
 
     end
