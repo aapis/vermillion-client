19c528e680c7798e2b16f186328c7543b58859cb
Ryan Priebe <rpriebe@me.com>
2016-04-21 16:05:01 -0600

    changed arguments so it sends server_name/remote_site_name instead of having like 4 arguments

--- lib/controllers/update.rb
+++ lib/controllers/update.rb
@@ -13,6 +13,9 @@ module Vermillion
           begin
             resp = @network.post(http + srv + '/api/update')
 
+            # generic failure for invalid response type
+            raise Errno::ECONNREFUSED if resp["Content-Type"] != "application/json"
+
             # we don't really care why this failed, just that it did
             raise Errno::ECONNREFUSED if resp.code.to_i > 399
 
@@ -27,18 +30,18 @@ module Vermillion
       end
 
       def one(input)
-        server = $config.get(:servers).select { |hash| hash['name'] == input }.first
+        server_name, remote_site = input.split('/')
+        server = $config.get(:servers).select { |hash| hash['name'] == server_name }.first
 
         # warn user if the server is not defined
-        return Notify.warning("Server not found: #{input}") unless server
+        return Notify.warning("Server not found: #{server_name}") unless server
 
         http = 'http://'
         http = 'https://' if server['https']
 
         begin
-          resp = @network.post(http + server['address'] + '/api/update')
+          resp = @network.post(http + server['address'] + '/api/update/' + remote_site)
           puts resp.body
-
           # generic failure for invalid response type
           raise Errno::ECONNREFUSED if resp["Content-Type"] != "application/json"
 
@@ -48,8 +51,9 @@ module Vermillion
           # handle JSON response
           response_data = JSON.parse(resp.body)
 
-          #Notify.success("#{response_data['_title']}: #{response_data['_message']}")
-          Notify.success(response_data['_data'])
+          if response_data['_code'] === 200
+            Notify.success("#{resp['Host']} updated")
+          end
         rescue Errno::ECONNREFUSED => e
           Notify.warning("Request failed for #{server['address']}")
         end
