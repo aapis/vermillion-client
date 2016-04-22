8965d99508ef0cfe95db8757f3d4c05022912e09
Ryan Priebe <rpriebe@me.com>
2016-04-22 10:36:59 -0600

    refactored so all commands call one of two methods

--- lib/controllers/update.rb
+++ lib/controllers/update.rb
@@ -3,34 +3,25 @@ module Vermillion
     class Update < Controller::Base
 
       def all
-        servers = $config.get(:servers)
-
-        $config.get(:servers).each do |hash|
-          srv = hash['address']
-          http = 'http://'
-          http = 'https://' if hash['https']
-
-          begin
-            resp = @network.post(http + srv + '/api/update')
-
-            # generic failure for invalid response type
-            raise Errno::ECONNREFUSED if resp["Content-Type"] != "application/json"
-
-            # we don't really care why this failed, just that it did
-            raise Errno::ECONNREFUSED if resp.code.to_i > 399
-
-            # handle JSON response
-            response_data = JSON.parse(resp.body)
-
-            Notify.success("#{response_data['_title']}: #{response_data['_message']}")
-          rescue Errno::ECONNREFUSED => e
-            Notify.warning("Request failed for #{srv}")
-          end
-        end
+        _send_to_all('/api/update/')
       end
 
-      def one(input)
-        server_name, remote_site = input.split('/')
+      def one(server)
+        _send_to_one(server, '/api/update/')
+      end
+
+      def config(server)
+        _send_to_one(server, '/api/update_config/')
+      end
+
+      private
+
+      def _send_to_one(input, endpoint)
+        # setup request values
+        server_name = input
+        remote_site = nil
+        server_name, remote_site = input.split('/') if input.include?('/')
+
         server = $config.get(:servers).select { |hash| hash['name'] == server_name }.first
 
         # warn user if the server is not defined
@@ -40,8 +31,13 @@ module Vermillion
         http = 'https://' if server['https']
 
         begin
-          resp = @network.post(http + server['address'] + '/api/update/' + remote_site)
+          if remote_site
+            resp = @network.post(http + server['address'] + endpoint + remote_site)
+          else
+            resp = @network.post(http + server['address'] + endpoint)
+          end
           puts resp.body
+
           # generic failure for invalid response type
           raise Errno::ECONNREFUSED if resp["Content-Type"] != "application/json"
 
@@ -56,6 +52,35 @@ module Vermillion
           end
         rescue Errno::ECONNREFUSED => e
           Notify.warning("Request failed for #{server['address']}")
+        end
+      end
+
+      def _send_to_all(endpoint)
+        servers = $config.get(:servers)
+
+        $config.get(:servers).each do |hash|
+          srv = hash['address']
+          http = 'http://'
+          http = 'https://' if hash['https']
+
+          begin
+            resp = @network.post(http + srv + endpoint)
+
+            # generic failure for invalid response type
+            raise Errno::ECONNREFUSED if resp["Content-Type"] != "application/json"
+
+            # we don't really care why this failed, just that it did
+            raise Errno::ECONNREFUSED if resp.code.to_i > 399
+
+            # handle JSON response
+            response_data = JSON.parse(resp.body)
+
+            if response_data['_code'] === 200
+              Notify.success("#{resp['Host']} updated")
+            end
+          rescue Errno::ECONNREFUSED => e
+            Notify.warning("Request failed for #{srv}")
+          end
         end
       end
 
