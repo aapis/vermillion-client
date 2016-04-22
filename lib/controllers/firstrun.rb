5f76f026102225d12c8a3a62a5179bc60ba6034e
Ryan Priebe <rpriebe@me.com>
2016-04-22 17:00:34 -0600

    added first run controller to create a config file with some sample servers

---
+++ lib/controllers/firstrun.rb
@@ -0,0 +1,29 @@
+module Vermillion
+  module Controller
+    class Firstrun < Controller::Base
+
+      def default
+        if File.exist?(Dir.home + '/.vermillion.yml')
+          Notify.error("Configuration already exists, this is not the first run!  Exiting.", show_time: false)
+        end
+
+        File.open(Dir.home + '/.vermillion.yml', "w") do |f|
+          f.write <<-'CONTENTS'
+servers:
+  - 
+    name: dev
+    address: 192.168.0.74
+    https: false
+    key: EDIT_ME
+  -
+    name: local
+    address: localhost:8000
+    https: false
+    key: EDIT_ME
+          CONTENTS
+        end
+      end
+
+    end
+  end
+end
