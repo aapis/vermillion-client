0562d1a9482e5bf95c5bc6043d3f55ab8c4f0b0a
Ryan Priebe <rpriebe@me.com>
2016-04-25 14:57:00 -0600

    added api communication helper which will hold all methods used to communicate with the api, other minor updates and refactoring

--- lib/controllers/change.rb
+++ lib/controllers/change.rb
@@ -1,6 +1,7 @@
 module Vermillion
   module Controller
     class Change < Controller::Base
+      include Helper::ApiCommunication
 
       def branch(server)
         send_to_one(server, :change_branch)
--- lib/controllers/update.rb
+++ lib/controllers/update.rb
@@ -1,6 +1,7 @@
 module Vermillion
   module Controller
     class Update < Controller::Base
+      include Helper::ApiCommunication
 
       def all
         send_to_all(:update)
