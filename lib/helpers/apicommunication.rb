e9da24587b0b1e951e9fbb1c533c97d5f7b0400d
Ryan Priebe <rpriebe@me.com>
2016-05-17 14:12:23 -0600

    refactored send_to_all method to better follow DRY

--- lib/helpers/apicommunication.rb
+++ lib/helpers/apicommunication.rb
@@ -10,12 +10,12 @@ module Vermillion
 
         endpoint = "/api/#{endpoint}/"
 
-        server = $config.get(:servers).select { |hash| hash['name'] == server_name }.first
+        server = $config.get(:servers).select { |hash| hash[:name] == server_name }.first
 
         # warn user if the server is not defined
         return Notify.warning("Server not found: #{server_name}") unless server
         # warn user if the site does not have a secret key property set
-        return Notify.warning("The server configuration must contain a key property to send requests") unless server['key']
+        return Notify.warning("The server configuration must contain a key property to send requests") unless server[:key]
         # warn user if the user key is not defined
         return Notify.warning("The configuration file must contain a user") unless $config.get(:user)
 
@@ -25,7 +25,7 @@ module Vermillion
         begin
           endpoint += remote_site if remote_site
           endpoint += "/#{other}" if other
-          resp = @network.post(http + server['address'] + endpoint, server['key'])
+          resp = @network.post(http + server[:address] + endpoint, server[:key])
           #puts http + server['address'] + endpoint
           puts resp.body
 
@@ -39,47 +39,18 @@ module Vermillion
           response_data = JSON.parse(resp.body)
 
           if response_data['_code'] === 200
-            Notify.success("#{server_name} (#{server['address']}) updated")
+            Notify.success("#{server_name} (#{server[:address]}) updated")
           end
         rescue Errno::ECONNREFUSED => e
-          Notify.warning("Request failed for #{server_name} (#{server['address']})")
+          Notify.warning("Request failed for #{server_name} (#{server[:address]})")
         end
       end
 
       def send_to_all(endpoint)
-        servers = $config.get(:servers)
-        endpoint = "/api/#{endpoint}"
+        servers = @format.symbolize($config.get(:servers))
 
         $config.get(:servers).each do |server|
-          srv = server['address']
-          http = 'http://'
-          http = 'https://' if server['https']
-
-          # warn user if the server is not defined
-          return Notify.warning("Server not found: #{server['name']}") unless server['name']
-          # warn user if the site does not have a secret key property set
-          return Notify.warning("The server configuration must contain a key property to send requests") unless server['key']
-          # warn user if the user key is not defined
-          return Notify.warning("The configuration file must contain a user") unless $config.get(:user)
-
-          begin
-            resp = @network.post(http + srv + endpoint, server['key'])
-
-            # generic failure for invalid response type
-            raise Errno::ECONNREFUSED if resp["Content-Type"] != "application/json"
-
-            # we don't really care why this failed, just that it did
-            raise Errno::ECONNREFUSED if resp.code.to_i > 399
-
-            # handle JSON response
-            response_data = JSON.parse(resp.body)
-
-            if response_data['_code'] === 200
-              Notify.success("#{resp['Host']} updated")
-            end
-          rescue Errno::ECONNREFUSED => e
-            Notify.warning("Request failed for #{srv}")
-          end
+          send_to_one(server[:name], endpoint) if server[:name]
         end
       end
 
--- lib/helpers/formatting.rb
+++ lib/helpers/formatting.rb
@@ -1,7 +1,29 @@
 module Vermillion
   module Helper
     class Formatting
-      
+
+      #
+      # Compatible with
+      # http://api.rubyonrails.org/classes/Hash.html#method-i-symbolize_keys
+      #
+      def symbolize(obj)
+        if obj.class == Array
+          # obj.map { |k| k.to_sym }
+          obj.each do |key|
+            #obj[(key.to_sym rescue key) || key] = obj.delete(key)
+            if key.class == Hash
+              symbolize(key)
+            else
+              key.map { |k| k.to_sym }
+            end
+          end
+        elsif obj.class == Hash
+          obj.keys.each do |key|
+            obj[(key.to_sym rescue key) || key] = obj.delete(key)
+          end
+        end
+      end
+
     end
   end
 end
