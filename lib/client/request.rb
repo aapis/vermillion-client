5ae429e5cff71469f669cb26ba423d68e0cd857c
Ryan Priebe <rpriebe@me.com>
2016-10-12 13:50:17 -0600

    fix rubocop issues in request

--- lib/client/request.rb
+++ lib/client/request.rb
@@ -15,19 +15,17 @@ module Vermillion
 
     # Create the request object, parse ARGV for values
     def initialize
-      @controller = nil
+      raise ArgumentError, "ARGV is empty" if ARGV.empty?
+
       @flags = ARGV.select { |f| f.start_with?('-') }.map { |f| f.split("=").map(&:to_sym) } || []
       @raw_flags = ARGV.select { |f| f.start_with?('-') } || []
+      @controller = ARGV[0].to_sym unless ARGV[0].start_with?('-')
+      @command = ARGV[1].to_sym unless ARGV[1].nil?
 
-      unless ARGV.empty?
-        @controller = ARGV[0].to_sym unless ARGV[0].start_with?('-')
-        @command = ARGV[1].to_sym unless ARGV[1].nil?
+      return unless ARGV.size > 2
 
-        if ARGV.size > 2
-          @custom = ARGV[2..ARGV.size].select { |p| !p.start_with?('-') }.map(&:to_sym) || []
-          @param = ARGV[2]
-        end
-      end
+      @custom = ARGV[2..ARGV.size].select { |p| !p.start_with?('-') }.map(&:to_sym) || []
+      @param = ARGV[2]
     end
   end
 end
