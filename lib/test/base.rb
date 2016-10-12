665398892bbf0c2600d698acde2b5fa699351b75
Ryan Priebe <rpriebe@me.com>
2016-10-12 11:58:34 -0600

    add and update more rdocs

--- lib/test/base.rb
+++ lib/test/base.rb
@@ -5,8 +5,10 @@ module Vermillion
     class Base < Minitest::Test
       include Vermillion::Controller
 
+      # Struct to mock the actual Request object
       MockRequest = Struct.new(:controller, :command, :param)
 
+      # Perform setup tasks
       def setup
         Notify.spit "Executing Tests (#{Time.now})"
       end
